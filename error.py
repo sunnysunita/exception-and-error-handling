cee77121b1c03ef15af85c73d08c91f6b2eee6f2
sunnysunita <sunnysunita.com@gmail.com>
2020-03-26 18:24:13 +0530

    day01

--- error.py
+++ error.py
@@ -1,2 +1,15 @@
-print("error and exception handing")
-print("error")
+while True:
+    try:
+        numerator=int(input("enter numerator: "))
+        denominator=int(input("enter denominator: "))
+        ans=numerator/denominator
+        print("answer: ",ans)
+        break
+    except ZeroDivisionError:
+        print("\t\t\tWARNING!!")
+        print("::denominator should not be equal to zero!!!::")
+        print("enter value again.")
+    except ValueError:
+        print("\t\t\tWARNING!!")
+        print("values should be integer.")
+        print("enter value again.")
